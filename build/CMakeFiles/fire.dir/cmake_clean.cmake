3350575e065c21fb9076e40431787e2baecd3aa2
ivan <ivan@debian>
2016-01-25 18:39:35 +0500

    remove build

---
+++ build/CMakeFiles/fire.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/fire.dir/detectors.cpp.o"
+  "CMakeFiles/fire.dir/main.cpp.o"
+  "fire.pdb"
+  "fire"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/fire.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
